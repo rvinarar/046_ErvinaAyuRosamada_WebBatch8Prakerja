f8b0b36b7ce206e05b3177531211280a7cd53c0b
rvinarar <ervinarosamada@gmail.com>
2023-07-07 10:42:07 +0700

    add file Sesi 6,7,8

---
+++ Sesi 6/latihan/dom.js
@@ -0,0 +1,14 @@
+document.write("Hello World");
+document.write("<h2>Saya sedang belajar DOM</h2>");
+
+// mengakses object head
+document.head;
+
+// mengakses object body
+document.body;
+
+// melihat jumlah huruf di judul pada object title
+document.title.length;
+
+// element DOM API dengan fungsi createElement()
+document.createElement('p');
